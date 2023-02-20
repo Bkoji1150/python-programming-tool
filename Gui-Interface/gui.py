3c4694c4c1f80753f7c56bbd1509ff08442c3b8e
apple <kojibello058@gmail.com>
2023-02-20 02:33:20 -0500

    Implemented the complete and exit

--- Gui-Interface/gui.py
+++ Gui-Interface/gui.py
@@ -5,10 +5,17 @@ import PySimpleGUI as sg  # Part 1 - The import
 # Define the window's contents
 layout = [[sg.Text("Type a to-do")],  # Part 2 - The Layout
           [sg.InputText(key="todo"), sg.Button("Add")],
+          [sg.Listbox(values=get_todos(),
+                      key='todos',
+                      enable_events=True,
+                      size=[45,10]),
+          sg.Button("Edit"), sg.Button("Complete")
+           ],
+          [sg.Button('Exit')]
           ]
 
 # Create the window
-window = sg.Window('My To-Do App', layout, font=('Helvetica', 20))
+window = sg.Window('My To-Do App', layout, font=('Helvetica', 20),)
 
 while True:
     event, values = window.read()
@@ -19,6 +26,26 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = get_todos()
+            todos.remove(todo_to_complete)
+            write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update('')
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 
---
+++ automated-email/main.py
@@ -0,0 +1,8 @@
+
+class Kojitechs:
+
+    def __int__(self,data):
+        self.data = data
+
+    def get(self):
+        pass
